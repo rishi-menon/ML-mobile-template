9a2c6cd9a31c57f077a7e8f87064061896900359
imashnake0 <krpaluru@uwaterloo.ca>
2024-09-28 13:56:14 -0400

    Add file name param and `runModel`

--- android/app/src/main/java/com/imashnake/aiels/Interpretter.kt
+++ android/app/src/main/java/com/imashnake/aiels/Interpretter.kt
@@ -2,6 +2,11 @@ package com.imashnake.aiels
 
 import android.content.Context
 import android.util.Log
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.withContext
 import org.tensorflow.lite.Interpreter
 import org.tensorflow.lite.support.common.FileUtil
 
@@ -9,10 +14,17 @@ private const val TAG = "Interpreter"
 
 /**
  * [Digit Classifier](https://github.com/google-ai-edge/litert-samples/blob/main/examples/digit_classifier/android/app/src/main/java/com/google/aiedge/examples/digit_classifier/DigitClassificationHelper.kt)
+ *
+ * @property fileName Name of the TFLite file (without `.tflite`)
  */
 class AielsInterpreter(
-    private val context: Context
+    private val fileName: String,
+    private val context: Context,
 ) {
+    val scaleAndSum: SharedFlow<Float>
+        get() = _scaleAndSum
+    private val _scaleAndSum = MutableSharedFlow<Float>()
+
     /** The TFLite interpreter instance.  */
     private var interpreter: Interpreter? = null
 
@@ -21,12 +33,21 @@ class AielsInterpreter(
     /** Init a Interpreter from the tflite asset */
     private fun initAielsInterpreter() {
         interpreter = try {
-            val litertBuffer = FileUtil.loadMappedFile(context, "digit_classifier.tflite")
+            val litertBuffer = FileUtil.loadMappedFile(context, "$fileName.tflite")
             Log.i(TAG, "Created TFLite buffer from asset")
             Interpreter(litertBuffer, Interpreter.Options())
         } catch (e: Exception) {
             Log.e(TAG, "Initializing TensorFlow Lite has failed with error: ${e.message}")
             return
         }
+    }
+
+    suspend fun runModel(
+        vector: Triple<Float, Float, Float>
+    ) = withContext(Dispatchers.IO) {
+        if (interpreter == null) return@withContext
+
+        val scaleAndSum = vector.toList().sum() * 2
+        _scaleAndSum.emit(scaleAndSum)
     }
 }
