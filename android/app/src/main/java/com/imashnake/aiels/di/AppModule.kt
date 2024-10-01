3326d901ab8cc1831bb85b9e1bdef1d400756325
imashnake0 <krpaluru@uwaterloo.ca>
2024-09-30 23:26:08 -0400

    Created `appModule`

--- android/app/src/main/java/com/imashnake/aiels/di/AppModule.kt
+++ android/app/src/main/java/com/imashnake/aiels/di/AppModule.kt
@@ -1,9 +1,12 @@
 package com.imashnake.aiels.di
 
 import com.imashnake.aiels.AielsInterpreter
+import com.imashnake.aiels.MainViewModel
 import org.koin.android.ext.koin.androidContext
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
-val utilsModule = module {
+val appModule = module {
     single { AielsInterpreter(fileName = "model", context = androidContext()) }
+    viewModel { MainViewModel(get()) }
 }
--- android/app/src/main/java/com/imashnake/aiels/di/ViewModelModule.kt
+++
@@ -1,9 +0,0 @@
-package com.imashnake.aiels.di
-
-import com.imashnake.aiels.MainViewModel
-import org.koin.core.module.dsl.viewModel
-import org.koin.dsl.module
-
-val viewModelModule = module {
-    viewModel { MainViewModel(get()) }
-}
